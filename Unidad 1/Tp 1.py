feb816795f03da6bd270702346cfec9193cc4846
tomyrc8 <tomasjabbur@hotmail.com>
2025-08-26 15:18:36 -0300

    Unidad 1 completa

---
+++ Unidad 1/Tp 1.py
@@ -0,0 +1,88 @@
+# Actividad 1: Crear un programa que imprima por pantalla el mensaje: “Hola Mundo!”: 
+# print ("Hola Mundo!")
+
+
+# Actividad 2: Crear un programa que pida al usuario su nombre e imprima por pantalla un saludo usando 
+# el nombre ingresado. Por ejemplo: si el usuario ingresa “Marcos”, el programa debe imprimir 
+# por pantalla “Hola Marcos!”. Consejo: esto será más sencillo si utilizas print(f…) para 
+# realizar la impresión por pantalla:
+# nombre = input ("Ingrese su nombre por favor: ")
+# print (f"Hola {nombre}!")
+
+
+# Actividad 3: Crear un programa que pida al usuario su nombre, apellido, edad y lugar de residencia e 
+# imprima por pantalla una oración con los datos ingresados. Por ejemplo: si el usuario ingresa 
+# “Marcos”, “Pérez”, “30” y “Argentina”, el programa debe imprimir “Soy Marcos Pérez, tengo 30 
+# años y vivo en Argentina”. Consejo: esto será más sencillo si utilizas print(f…) para realizar 
+# la impresión por pantalla:
+# nombre = input ("Ingrese su nombre por favor: ")
+# edad = int (input("Ingrese su edad por favor: "))
+# residencia = input ("En que lugar del mundo reside? ")
+# print (f"Soy {nombre}, tengo {edad} años y vivo en {residencia}
+
+
+# Actividad 4: Crear un programa que pida al usuario el radio de un círculo e imprima por pantalla su área y 
+#su perímetro:
+# radio = float(input("Ingrese el radio del circulo: "))
+# pi = 3.14159
+# area= pi*radio*radio
+# perimetro = 2*pi*radio
+# print (f"El area del circulo es {area} y su perimetro equivale a {perimetro}")
+
+
+# Actividad 5: Crear un programa que pida al usuario una cantidad de segundos e imprima por pantalla a 
+# cuántas horas equivale:
+# segundos = float (input ("ingrese una cierta cantidad de segundos:"))
+# horas = segundos/60
+# print (f"{segundos} segundos equivale a {horas} horas")
+
+
+# Actividad 6: Crear un programa que pida al usuario un número e imprima por pantalla la tabla de 
+# multiplicar de dicho número:
+# numero = float (input ("Ingrese un numero: "))
+# print (numero * 1)
+# print (numero * 2)
+# print (numero * 3)
+# print (numero * 4)
+# print (numero * 5)
+# print (numero * 6)
+# print (numero * 7)
+# print (numero * 8)
+# print (numero * 9)
+# print (numero * 10)
+
+
+# Actividad 7: Crear un programa que pida al usuario dos números enteros distintos del 0 y muestre por 
+# pantalla el resultado de sumarlos, dividirlos, multiplicarlos y restarlos:
+# numero1 = int (input ("Ingrese un numero entero distinto del 0: "))
+# numero2 = int (input ("Ingrese otro numero entero distinto del 0 y diferente al primero: "))
+# suma = numero1+numero2
+# resta = numero1-numero2
+# multiplicacion = numero1*numero2
+# division = numero1/numero2
+# print (f"suma = {suma}, resta = {resta}, multiplicacion = {multiplicacion}, division = {division}")
+
+
+# Actividad 8: Crear un programa que pida al usuario su altura y su peso e imprima por pantalla su índice 
+# de masa corporal. Tener en cuenta que el índice de masa corporal se calcula del siguiente 
+# modo: IMC = peso en kg / (altura en m)2:
+# altura = float (input ("Ingrese su altura en metros: "))
+# peso = float (input ("Ingrese su peso corporal en kilogramos: "))
+# imc = peso / (altura*altura)
+# print (f"El IMC del usuario es igual a {imc} kg/m2")
+
+
+# Actividad 9: Crear un programa que pida al usuario una temperatura en grados Celsius e imprima por pantalla su equivalente en grados Fahrenheit. 
+# Tener en cuenta la siguiente equivalencia: t𝑒𝑚𝑝𝑒𝑟𝑎𝑡𝑢𝑟𝑎 𝑒𝑛 𝐹𝑎ℎ𝑟𝑒𝑛ℎ𝑒it = temp en celcius * (9/5) + 32 :
+# temp_celcius = float (input("Ingrese la temperatura en grados celcius: "))
+# temp_fahrenheit = temp_celcius * (9/5) + 32
+# print (f"La temperatura ingresada en grados celcius equivale a {temp_fahrenheit} grados fahrenheit")
+
+
+# Actividad 10: Crear un programa que pida al usuario 3 números e imprima por pantalla el promedio de 
+# dichos números:
+# numero_1 = float (input ("Ingrese un primer numero: "))
+# numero_2 = float (input ("Ingrese un segundo numero: "))
+# numero_3 = float (input ("Ingrese un tercer numero: "))
+# promedio = (numero_1+numero_2+numero_3)/3
+# print (f"El promedio de los numeros ingresados es igual a: {promedio}")
